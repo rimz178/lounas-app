152db8fbc4f6eee4036e1013d05f756c3c3e4c97
Riku <riku.mikkonen@tuni.fi>
2026-03-02 17:18:07 +0200

    feat: enhance homepage layout with improved header and dark mode styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-
 import HomeClient from "./components/HomeClient";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="p-4 flex items-center justify-between">
-        <h1 className="text-3xl font-bold">Lounas Tänään</h1>
-      </div>
+      <header className="w-full bg-orange-200 py-20 flex flex-col items-center justify-center">
+        <h1 className="text-7xl font-bold text-center mb-4">Lounas Tänään</h1>
+        <p className="text-3xl text-center">
+          Lounastänään.fi – löydä päivän lounas!
+        </p>
+      </header>
       <HomeClient />
     </div>
   );
